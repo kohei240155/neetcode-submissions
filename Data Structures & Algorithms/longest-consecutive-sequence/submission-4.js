7158272161fab103d473559cc714cd689de164df
Kohei Fujiwara <117567768+kohei240155@users.noreply.github.com>
2026-06-09 14:17:49 -0700

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const numSet = new Set(nums);
+        let longest = 0;
+
+        for (const num of numSet) {
+            if (!numSet.has(num - 1)) {
+                let length = 1;
+
+                while (numSet.has(num + length)) {
+                    length++;
+                }
+                longest = Math.max(length, longest);
+            }
+        }
+
+        return longest
+    }
+}
