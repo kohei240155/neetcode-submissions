65ae69fdf9d49c79c750b6566505865194283780
Kohei Fujiwara <117567768+kohei240155@users.noreply.github.com>
2026-06-11 12:26:27 -0700

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        const pair = {
+            '}': '{',
+            ']': '[',
+            ')': '(',
+        }
+
+        for (const char of s) {
+            console.log(char);
+            if (char === '{' || char === '[' || char == '(') {
+                stack.push(char);
+            } else {
+                if (pair[char] !== stack.pop()) {
+                    return false;
+                }
+            }
+        }
+        return stack.length === 0;
+    }
+}
